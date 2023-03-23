754f0de7a443174cd38afe7272465d684edb0e51
natopo99 <104119337+natopo99@users.noreply.github.com>
2023-03-23 17:37:27 -0600

    updates

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactDOM from 'react-dom';
+
+
+
+import data from "./teams.json";
+const teamList = data.teams;
+
+function Welcome(){
+  return(<h1>It Worked!!!</h1>);
+}
+
+class Teams extends React.Component{
+  render()
+  {
+    const oneTeam = this.props;
+    return(
+      <div>
+        <h2>{oneTeam.school}</h2>
+        <h2>{oneTeam.name}</h2>
+        <h2>{oneTeam.state}</h2>
+        <h2>{oneTeam.city}</h2>
+      </div>
+    );
+  }
+}
+
+function TeamInfo(){
+  return(
+    <div>
+      {teamList.map(oneTeam => <Teams {...oneTeam}/>)}
+    </div>
+  );
+}
+
+
 
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        <Welcome/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          
+          Edit <code>src/App.
+          </code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <TeamInfo/>
     </div>
   );
 }
+ReactDOM.render(
+  <App/>,
+  document.getElementById('mountNode'),              
+);
 
 export default App;
